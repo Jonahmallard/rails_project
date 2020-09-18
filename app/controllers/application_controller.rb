8a558bd1cd28e59a8381b44ed1bb36d54ca93823
jonah <benjaminjmallard@gmail.com>
2020-09-18 17:52:45 -0500

    Added Specs/Guidelines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def current_driver
+        @current_driver ||= Driver.first
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Faker
+
+# 1000.times do
+#     Drive.create()
+# end
+
+#at a minimum
+# create one driver
+
+# create 3-5 cars
+
+# 10.times do
+#     car = Car.all.sample
+#     drive = Drive.first
+#     placement = rand(1..15)
+#     Race.create(car_id: car.id, drive_id: drive.id, location: "race track", placement: placement)
+    
+# end
