40bca5e0a040cce1bd8274bb8810d74134208928
jonah <benjaminjmallard@gmail.com>
2020-09-23 19:30:56 -0500

    Races/Cars index/view all buttons

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -2,4 +2,12 @@ class RacesController < ApplicationController
     def index
         @races = Race.all
     end
+
+    def show
+        @race = Race.find_by_id(params[:id])
+    end
+
+    #def race_params
+     #   params.require(:race).permit(:action, :id)
+    #end
 end
